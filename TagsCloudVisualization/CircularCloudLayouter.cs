19a3a426927187d9aac34ba4ca66b09e19ceeb75
Eien_g <GSKryachko@gmail.com>
2017-11-09 13:57:49 +0500

    Now i'm satisfied with layout

--- TagsCloudVisualization/CircularCloudLayouter.cs
+++ TagsCloudVisualization/CircularCloudLayouter.cs
@@ -4,8 +4,7 @@ using System.Drawing;
 using System.Linq;
 using NUnit.Framework;
 
-namespace TagsCloudVisualization
-{
+namespace TagsCloudVisualization {
     public class CircularCloudLayouter
     {
         private Point center;
@@ -25,29 +24,32 @@ namespace TagsCloudVisualization
 
         public Rectangle PutNextRectangle(Size size)
         {
-//            var point = new Point(mainRectangle.Right,center.Y);
-//                         
-//            var newRect = new Rectangle(point,size);
-//            rectangles.Add (new Rectangle(center, size));
-//
-//
-//            mainRectangle = mainRectangle == null? newRect: Rectangle.Union(mainRectangle,newRect);
-            var position = CalculateFermatSpiral( center);
+            //            var point = new Point(mainRectangle.Right,center.Y);
+            //                         
+            //            var newRect = new Rectangle(point,size);
+            //            rectangles.Add (new Rectangle(center, size));
+            //
+            //
+            //            mainRectangle = mainRectangle == null? newRect: Rectangle.Union(mainRectangle,newRect);
+            angle = 0;
+            var position = CalculateFermatSpiral(center);
+            
             var newRect = MoveRectToItsCenter(new Rectangle(position, size));
             while (intestectsWithOther(newRect))
             {
-                 position = CalculateFermatSpiral( center);
+                position = CalculateFermatSpiral(center);
                 newRect = MoveRectToItsCenter(new Rectangle(position, size));
                 Console.WriteLine(angle);
             }
+            newRect = MoveRectToCenterUsingVector2(newRect);
             rectangles.Add(newRect);
             return newRect;
         }
 
-//        private Rectangle PutRectangleIntoRandomEmptyPosition(Rectangle rect)
-//        {
-//            
-//        }
+        //        private Rectangle PutRectangleIntoRandomEmptyPosition(Rectangle rect)
+        //        {
+        //            
+        //        }
 
         private Rectangle MoveRectToItsCenter(Rectangle rect)
         {
@@ -55,16 +57,102 @@ namespace TagsCloudVisualization
             rect.Y -= rect.Size.Height / 2;
             return rect;
         }
-        private Point CalculateFermatSpiral(Point offset) {
+
+        private Point CalculateFermatSpiral(Point offset)
+        {
             angle += 250;
-            var x = (int)(Math.Sqrt(angle) * Math.Cos(angle));
-            var y = (int)(Math.Sqrt(angle) * Math.Sin(angle));
+            var x = (int) (Math.Sqrt(angle*2) * Math.Cos(angle));
+            var y = (int) (Math.Sqrt(angle) * Math.Sin(angle));
             return new Point(x + offset.X, y + offset.Y);
         }
 
         private bool intestectsWithOther(Rectangle rect)
         {
-            return rectangles.Any((r) => r.IntersectsWith(rect));
+            return rect.IsEmpty || rectangles.Any((r) => r.IntersectsWith(rect));
+        }
+       
+
+        private Point Normalize(Point vector)
+        {
+            var length = GetVectorLength(vector);
+            return new Point((int) (vector.X / length), (int) (vector.Y / length));
+        }
+
+        private Point addVector(Point a, Point b)
+        {
+            return new Point(a.X + b.X, a.Y + b.Y);
+        }
+
+        private Point substractVector(Point a, Point b)
+        {
+            return new Point(a.X - b.X, a.Y - b.Y);
+        }
+
+        private double GetVectorLength(Point vector)
+        {
+            return Math.Sqrt(Math.Pow(vector.X, 2) + Math.Pow(vector.Y, 2));
+        }
+
+        private Rectangle MoveRectToCenterUsingVector(Rectangle rect)
+        {
+            var vector = new Point(center.X - rect.X, center.Y - rect.Y);
+            var unitVector = Normalize(vector);
+            for (var i = 0; i < 30; i++)
+            {
+                rect.Location = addVector(rect.Location, unitVector);
+                if (intestectsWithOther(rect))
+                {
+                    rect.Location = substractVector(rect.Location, unitVector);
+                    break;
+                }
+
+            }
+            vector = new Point(0, center.Y - rect.Y);
+            unitVector = Normalize(vector);
+            for (var i = 0; i < 60; i++)
+            {
+                rect.Location = addVector(rect.Location, unitVector);
+                if (intestectsWithOther(rect))
+                {
+                    rect.Location = substractVector(rect.Location, unitVector);
+                    break;
+                }
+            }
+            vector = new Point(center.X - rect.X, 0);
+            unitVector = Normalize(vector);
+            for (var i = 0; i < 60; i++)
+            {
+                rect.Location = addVector(rect.Location, unitVector);
+                if (intestectsWithOther(rect))
+                {
+                    rect.Location = substractVector(rect.Location, unitVector);
+                    break;
+                }
+
+            }
+            return rect;
+
+        }
+
+        private Rectangle MoveRectToCenterUsingVector2(Rectangle rect)
+        {
+            for (var i = 0; i < 30000; i++) {
+                var vector = new Point((center.X - rect.X) / 2 +1, (center.Y - rect.Y) / 2 +1);
+
+                rect.Location = addVector(rect.Location, vector);
+                if (intestectsWithOther(rect))
+                {
+                    rect.Location = substractVector(rect.Location, vector);
+                    break;
+                }
+
+            }
+
+           
+
+            return rect;
+
         }
     }
 }
+    
--- TagsCloudVisualization/CloudVisualizer.cs
+++ TagsCloudVisualization/CloudVisualizer.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Drawing;
+using System.Linq;
+using NUnit.Framework;
 
 namespace TagsCloudVisualization
 {
@@ -19,9 +22,14 @@ namespace TagsCloudVisualization
             var bitmap = new Bitmap(400,400);
             var graphics = Graphics.FromImage(bitmap);
 
-            for (var i = 0; i < 30; i++)
+//            for (var i = 0; i < 50; i++)
+//            {
+//                graphics.DrawRectangle(new Pen(Color.Cyan), layouter.PutNextRectangle(GenerateRandomSize()));
+//            }
+
+            foreach (var randomSize in GenerateSortedListOfRandomSizes(50))
             {
-                graphics.DrawRectangle(new Pen(Color.Cyan), layouter.PutNextRectangle(GenerateRandomSize()));
+                graphics.DrawRectangle(new Pen(Color.Cyan), layouter.PutNextRectangle(randomSize));
             }
 
             bitmap.Save("savedTest.bmp");
@@ -31,6 +39,17 @@ namespace TagsCloudVisualization
         private Size GenerateRandomSize()
         {
             return new Size(rnd.Next(40,100),rnd.Next(10,20));
+        }
+
+        private List<Size> GenerateSortedListOfRandomSizes(int size)
+        {
+            var rects = new List<Size>();
+            for (var i = 0; i < size; i++)
+            {
+                rects.Add(GenerateRandomSize());
+            }
+
+            return rects.OrderByDescending(x => x.Height * x.Width).ToList();
         }
     }
 }
